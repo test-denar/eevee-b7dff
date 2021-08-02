d14247c8f7cbb4189e793b75a5c44275f5b226b4
Stackbit Code Editor <projects@stackbit.com>
2021-08-02 03:36:25 +0000

    FeaturesSection.js: changed

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -44,7 +44,7 @@ export default class FeaturesSection extends React.Component {
                     )}
                 </div>
                 <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-                 <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="elisabeth-vehling" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/elisabeth-vehling?trk=profile-badge">Elisabeth Vehling</a></div>
+                <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="elisabeth-vehling" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/elisabeth-vehling?trk=profile-badge">Elisabeth Vehling</a></div>
             </div>
         )
     }
